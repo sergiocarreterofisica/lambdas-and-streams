39db50327fae97ffa59a86ce8a1fb7238efc6a8a
Sergio Carretero Peña <sergiocarreterofisica@gmail.com>
2023-12-07 01:59:31 +0100

    Punto 7 del curso. Apartado de operadores intermedios.

---
+++ src/main/java/com/debuggeando_ideas/streams/ExerciseLambdas.java
@@ -0,0 +1,105 @@
+package com.debuggeando_ideas.streams;
+
+import com.debuggeando_ideas.util.Console;
+import com.debuggeando_ideas.util.Database;
+import com.debuggeando_ideas.util.Review;
+import com.debuggeando_ideas.util.Videogame;
+
+import java.util.Comparator;
+import java.util.NoSuchElementException;
+import java.util.stream.Stream;
+
+public class ExerciseLambdas {
+
+	public static void main(String[] args) {
+
+		System.out.println(exercise1(Database.videogames.stream()));
+
+		exercise2(Database.videogames.stream()).forEach(System.out::println);
+		exercise2(Database.videogames.stream());
+
+		System.out.println(exercise3(Database.videogames.stream()));
+		
+		exercise4(Database.videogames.stream()).forEach(System.out::println);
+		
+		exercise5(Database.videogames.stream()).forEach(System.out::println);
+		
+		exercise6(Database.videogames.stream()).forEach(System.out::println);
+		
+	}
+
+	/*
+	 * Regresar true si el stream contiene al menos un videojuego con número de
+	 * ventas mayor a 10 y no este en descuento o su precio sea mayor a 9.99 de lo
+	 * contrario regresar false.
+	 */
+	static Boolean exercise1(Stream<Videogame> stream) {
+
+		Long ejercicio1 = stream.filter(v -> v.getTotalSold() > 10 && (!v.getIsDiscount() || v.getPrice() > 9.99)).count();
+
+		return ejercicio1.intValue() > 1;
+
+//		return stream.anyMatch(v -> v.getTotalSold() > 10 && (!v.getIsDiscount() || v.getPrice() > 9.99));
+	}
+
+	/*
+	 * Regresar un Stream unicamente con los titulos de todas las consolas que sean
+	 * de XBOX durante el proceso imprimir los resultados ignorando los repetidos.
+	 */
+	static Stream<String> exercise2(Stream<Videogame> stream) {
+
+		System.out.println("-------------------------------");
+
+		return stream.filter(v -> v.getConsole().equals(Console.XBOX)).distinct().map(Videogame::getName)
+				.peek(System.out::println);
+
+	}
+
+	/*
+	 * Regresar el videojuego con el mayor número de ventas unicamente contemplando
+	 * los primers 10 elementos del stream.
+	 */
+	static Videogame exercise3(Stream<Videogame> stream) {
+
+		System.out.println("-------------------------------");
+
+		return stream.limit(10).max(Comparator.comparing(Videogame::getTotalSold))
+				.orElseThrow(NoSuchElementException::new);
+
+//		return stream.limit(10).max((v1, v2) -> v1.getTotalSold().compareTo(v2.getTotalSold())).get();
+	}
+
+	/*
+	 * Regresar un stream con todos los comentarios del cada review de todos los
+	 * videojuegos del stream.
+	 */
+	static Stream<String> exercise4(Stream<Videogame> stream) {
+
+		System.out.println("-------------------------------");
+		
+		return stream.flatMap(v -> v.getReviews().stream().map(Review::getComment));
+	}
+
+	/*
+	 * Regresar un stream con los todos los videojuegos con precio menores a 20.0
+	 * sin utilizar el operador filter().
+	 */
+	static Stream<Double> exercise5(Stream<Videogame> stream) {
+		
+		System.out.println("-------------------------------");
+		
+		return stream.sorted(Comparator.comparingDouble(Videogame::getPrice)).takeWhile(v -> v.getPrice() < 20.0).map(Videogame::getPrice);
+	}
+	
+	/*
+	 * Regresar un stream con los todos los videojuegos con precio menores a 20.0
+	 * sin utilizar el operador filter().
+	 */
+	static Stream<Double> exercise6(Stream<Videogame> stream) {
+		
+		System.out.println("-------------------------------");
+		
+		return stream.sorted(Comparator.comparingDouble(Videogame::getPrice).reversed()).takeWhile(v -> v.getPrice() >= 20.0).map(Videogame::getPrice);
+	}
+
+}
---
+++ src/main/java/com/debuggeando_ideas/streams/StreamIntermediatOperators.java
@@ -0,0 +1,146 @@
+package com.debuggeando_ideas.streams;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import com.debuggeando_ideas.util.BasicVideogame;
+import com.debuggeando_ideas.util.Database;
+import com.debuggeando_ideas.util.Videogame;
+
+public class StreamIntermediatOperators {
+
+	public static void main(String[] args) {
+
+		Stream<Videogame> videogames = Database.videogames.stream();
+
+		System.out.println(videogames.count());
+
+		distinctOperator(Database.videogames.stream());
+
+		limitOperator(Database.videogames.stream());
+
+		skipOperator(Database.videogames.stream());
+
+		filterOperator(Database.videogames.stream());
+
+		mapOperator(Database.videogames.stream());
+
+		flatMapOperator(Database.videogames.stream());
+
+		mapVsflatMapOperator(Database.videogames.stream());
+
+//		peekOperator(Database.videogames.stream());
+
+		sortOperator(Database.videogames.stream());
+
+		takeWhileOperator(Database.videogames.stream());
+
+		dropWhileOperator(Database.videogames.stream());
+
+	}
+
+	static void distinctOperator(Stream<Videogame> videogames) {
+		System.out.println("-------------------------------------------");
+		System.out.println(videogames.distinct().count());
+	}
+
+	static void limitOperator(Stream<Videogame> videogames) {
+		System.out.println("-------------------------------------------");
+		List<Videogame> r = videogames.limit(15).collect(Collectors.toList());
+		r.forEach(System.out::println);
+	}
+
+	static void skipOperator(Stream<Videogame> videogames) {
+		System.out.println("-------------------------------------------");
+		List<Videogame> r = videogames.skip(10).collect(Collectors.toList());
+		r.forEach(System.out::println);
+	}
+
+	static void filterOperator(Stream<Videogame> videogames) {
+
+		System.out.println("-------------------------------------------");
+		List<Videogame> r = videogames.filter(v -> v.getPrice() > 5.0).filter(v -> !v.getIsDiscount())
+				.filter(v -> v.getOfficialWebsite().contains("forza")).collect(Collectors.toList());
+
+		r.forEach(System.out::println);
+
+	}
+
+	static void mapOperator(Stream<Videogame> videogames) {
+
+		System.out.println("-------------------------------------------");
+		List<BasicVideogame> basicVideogames = videogames.map(
+				v -> BasicVideogame.builder().name(v.getName()).price(v.getPrice()).console(v.getConsole()).build())
+				.collect(Collectors.toList());
+
+		basicVideogames.forEach(System.out::println);
+
+//		List<String> titles = basicVideogames.stream().map(v -> v.getName()).collect(Collectors.toList());
+		List<String> titles = basicVideogames.stream().map(BasicVideogame::getName).collect(Collectors.toList());
+
+		titles.forEach(System.out::println);
+	}
+
+	static void flatMapOperator(Stream<Videogame> videogames) {
+
+		System.out.println("-------------------------------------------");
+
+		var r = videogames.flatMap(v -> v.getReviews().stream()).collect(Collectors.toList());
+		r.forEach(System.out::println);
+
+	}
+
+	static void mapVsflatMapOperator(Stream<Videogame> videogames) {
+
+		System.out.println("-------------------------------------------");
+
+//		var totalReviews = videogames.map(v -> v.getReviews().size()).collect(Collectors.toList());
+		var totalReviews = videogames.flatMap(v -> v.getReviews().stream()).count();
+
+//		System.out.println(totalReviews.size());
+		System.out.println(totalReviews);
+
+	}
+
+	static void peekOperator(Stream<Videogame> videogames) {
+		System.out.println("-------------------------------------------");
+		videogames.peek(v -> v.setName("Empty")).forEach(System.out::println);
+	}
+
+	static void sortOperator(Stream<Videogame> videogames) {
+		System.out.println("------------------------------------------");
+
+		List<Videogame> listSorted = videogames.sorted(Comparator.comparingInt(v -> v.getReviews().size()))
+				.collect(Collectors.toList());
+
+		listSorted.forEach(System.out::println);
+
+	}
+
+	static void takeWhileOperator(Stream<Videogame> videogames) {
+		System.out.println("------------------------------------------");
+
+		List<Videogame> r = videogames
+				.sorted(Comparator.comparing(Videogame::getName))
+				.takeWhile(v -> !v.getName().startsWith("M"))
+				.collect(Collectors.toList());
+
+		r.forEach(System.out::println);
+
+	}
+
+	static void dropWhileOperator(Stream<Videogame> videogames) {
+		System.out.println("------------------------------------------");
+
+		List<Videogame> r = videogames
+				.sorted(Comparator.comparing(Videogame::getName))
+				.dropWhile(v -> !v.getName().startsWith("M"))
+				.collect(Collectors.toList());
+
+		r.forEach(System.out::println);
+
+	}
+
+}
